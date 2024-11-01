2f1328110b3f87a9bbc81feee2191a01e9cf61d7
Jorenar <dev@jorenar.com>
2024-11-01 18:23:06 +0100

    Update LICENSE

--- cmoball.h
+++ cmoball.h
@@ -1,5 +1,6 @@
 /*
- * Copyright 2021 Jorengarenar <dev@joren.ga>
+ * SPDX-License-Identifier: MIT
+ * Copyright 2021,2024 Jorenar <dev@jorenar.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
